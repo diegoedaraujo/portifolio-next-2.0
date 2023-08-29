c9868010ae3a63f8b544b64908577e3fa331ee0c
Diego Araújo <diegoedaraujo@gmail.com>
2023-08-29 11:22:21 -0300

    Implementing Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,27 @@
-import Image from 'next/image'
-import Head from 'next/head'
-import { NextPage } from 'next'
-import Header from '@/components/Header'
-import Hero from '@/components/Hero'
+import Image from "next/image";
+import Head from "next/head";
+import { NextPage } from "next";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
-const Home: NextPage = ()=> {
+const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory 
+    overflow-scroll z-0">
       <Head>
         <title>{`Diego's Portifolio`}</title>
       </Head>
 
       <Header />
 
-      <section id='hero'>
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
-    </div>    
-  )
-}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
 
 // export default function Home() {
 //   return (
